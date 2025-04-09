320ca60889a3fc7c195dfa9a341b434b93c49ecd
Sangit Manandhar <rux12@icloud.com>
2025-04-08 22:21:41 -0400

    chore: add .gitignore

--- dist/usage.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DEFAULT_USAGE = void 0;
-exports.DEFAULT_USAGE = `
-USAGE : node app.js <PROJECTS_PATH>
-
-<PROJECTS_PATH> -- Path where the units(projects) are stored.
-`;
-//# sourceMappingURL=usage.js.map
