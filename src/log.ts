649ea56d48045cb1327978bc164124961f10e90f
Sangit Manandhar <rux12@icloud.com>
2025-04-09 14:57:19 -0400

    chore: update log file

--- src/log.ts
+++ src/log.ts
@@ -8,19 +8,19 @@ export interface Log {
 }
 
 const APP_NAME = styleText("blue", "CWS")
-const ERROR_TEXT = styleText("red", "ERROR")
+const ERROR_TEXT = styleText("red", "ERR")
 const WARN_TEXT = styleText("yellow", "WARN")
 
 export const log = {
     error: (message: any) => {
-        console.error(`[${APP_NAME}/${ERROR_TEXT}]: ${message}`)
+        console.error(`[${APP_NAME}][${ERROR_TEXT}]: ${message}`)
     },
     error_and_exit: (message: any) => {
-        console.error(`[${APP_NAME}/${ERROR_TEXT}]: ${message}`)
+        console.error(`[${APP_NAME}][${ERROR_TEXT}]: ${message}`)
         process.exit(1)
     },
     warn: (message: any) => {
-        console.warn(`[${APP_NAME}/${WARN_TEXT}]: ${message}`)
+        console.warn(`[${APP_NAME}][${WARN_TEXT}]: ${message}`)
     },
     log: (message: any) => {
         console.log(`[${APP_NAME}]: ${message}`)
