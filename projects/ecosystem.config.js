1ec498627c48f3b825dfef937b06655be66d4bd7
Sangit Manandhar <rux12@icloud.com>
2025-04-08 22:22:49 -0400

    chore: change projects to examples

--- projects/ecosystem.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  apps: [
-    { cwd: "test1", name: "test_one", script: "test", env: { PORT: 30055 } },
-    { cwd: "test2", name: "test_two", script: "test", env: { PORT: 3000 } },
-  ],
-};
