e810ed498a4a079b94e54299e74f5e49634b6a29
Sangit Manandhar <rux12@icloud.com>
2025-04-09 17:54:05 -0400

    chore: change pm2 restart

--- src/daemon/pm2/index.ts
+++ src/daemon/pm2/index.ts
@@ -7,11 +7,15 @@ import { execSync } from "node:child_process";
 
 const PM2Port = {
     _reset_pm2: () => {
-        execSync("pm2 stop all")
-        execSync("pm2 del all")
+        execSync(`pm2 jlist | jq -r '.[] | select(.name != "ROOT") | .pm_id' | xargs -n1 pm2 delete`)
     },
     _start_pm2: (filePath: string) => {
-        execSync(`pm2 start ${filePath}`)
+        execSync(`
+             for app in $(pm2 jlist | jq -r '.[] | select(.pm2_env.status == "stopped") | .name'); do
+                pm2 start ${filePath} --only "$app"
+             done
+        `
+        )
     },
     run: (rootPath: string) => {
         const validUnitsConfigs = getValidUnitConfigs({ log, rootPath, validateConfig, CONFIG_FILE_NAME: UNIT_CONFIG_FILE_NAME })
